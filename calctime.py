17d8fd3a7a5a1eaaf1465527eed671d00b5d8225
Diogo S. Lourenço <114771411+diogolourenco100@users.noreply.github.com>
2025-03-10 13:44:42 +0000

    new file:   calctime.py

---
+++ calctime.py
@@ -0,0 +1,11 @@
+import time
+
+def Calctime(function]):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = function](*args, **kwargs)
+        end = time.time()
+        execution_time = end - start
+        print(f"Execution time: {execution_time:.2f} seconds.")
+        return result
+    return wrapper
